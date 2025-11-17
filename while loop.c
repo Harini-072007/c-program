a820069dedc14d39daa95edd8af5d959a3df0316
Harini-072007 <harinib051@gmail.com>
2025-11-17 19:15:00 +0530

    Add files via upload

---
+++ while loop.c
@@ -0,0 +1,16 @@
+
+#include<stdio.h>
+int main()
+{
+    int n,i=1,sum=0;
+    printf("Enter the positive integer:");
+    scanf("%d,&n");
+    while(i<=n)
+    {
+        sum=sum+i;
+        i++;
+
+    }
+    printf("Sum=%d",sum);
+    return 0;
+}
