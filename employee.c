43fd3bd2cadd1f8009cab08bba115da97d0dd19b
Harini-072007 <harinib051@gmail.com>
2025-12-01 11:46:22 +0530

    Add files via upload

---
+++ employee.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+int id;
+char name[20];
+float salary;
+};
+int main(){
+  Employee e1={1001."kiran",45000,50};
+  printf("Employee id:%d\n",e1.id);
+  printf("name:%s\n",e1.name);
+  printf("salary:%d\n",e1.salary);
+  return 0;
+}
+
+
